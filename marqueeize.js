7935f31945a988413785d2c280066177d3a13e81
DaveyWood <dwood@cvent.com>
2022-03-18 19:50:47 -0400

    Remove jQuery dependency

--- marqueeize.js
+++ marqueeize.js
@@ -1,4 +1,17 @@
-if(jQuery){
-var divs = jQuery('div');
-for(var i = 0, div; div = divs[i++];){var stuff = jQuery(div).children().detach(); var m = jQuery('<marquee/>').attr('scrollamount', 10).attr('truespeed', 'truespeed').attr('scrolldelay', 40).attr('behavior', 'alternate'); stuff.appendTo(m); m.appendTo(jQuery(div))};
-}
+(function() {
+  const marqueeize = () => {
+    const divs = document.querySelectorAll('div');
+    for (let i = 0, div; (div = divs[i++]); ) {
+      let stuff = [...div.children];
+      let m = document.createElement("marquee");
+      m.setAttribute("scrollamount", 10)
+      m.setAttribute("truespeed", "truespeed")
+      m.setAttribute("scrolldelay", 40)
+      m.setAttribute("behavior", "alternate");
+      stuff.forEach(child => m.appendChild(child));
+      div.appendChild(m);
+    }
+  }
+
+  window.setTimeout(marqueeize, 30_000);
+})();
